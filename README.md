ff2f3941d3e85c144d0bb0f84797ed1fa0dfd727
grenudi <idunerg@gmail.com>
2022-06-19 23:39:28 +0300

    retarget for vs2022; reduce cpu usage in idle; +window_name; readme

--- README.md
+++ README.md
@@ -8,18 +8,19 @@
  Go to actions and select the first one in the list. Then there should be a list of downloads. I recommend the Release x86 one, but all of them work. Don't use the Debug ones, unless you experience errors, as the Debug build run a lot slower.
 
 ## Running
- `l4d2-autobhop <playerbase> <mFlags offset>`
+ `l4d2-autobhop.exe <playerbase> <mFlags offset> <window name>`
  
  I use these offsets, but they can change if the game recieves an update:
  ```
- player base = 0x6FD9D8
+ player base = 0x724B58
  mFlags offset = 0xF0 
  ```
+ Window name sometimes differ as well.  
  If these doesnt work you can find other ones on the unknowncheats forum.
  
  This is the commandline i use:
  
- `l4d2-autobhop 0x6FD9D8 0xF0`
+ `l4d2-autobhop.exe 0x724B58 0xF0 "Left 4 Dead 2 - Direct3D 9"`
 
 ## Building 
- Open this project in Visual Studio 2019. Select the configuration and platform and hit build.
+ Open this project in Visual Studio 2022. Select the configuration and platform and hit build.
--- main.c
+++ main.c
@@ -6,94 +6,104 @@
 #include <TlHelp32.h>
 #include <Windows.h>
 
-int main(const int argc, char *argv[]) {
-  // Print syntax and exit program if we don't have the required amounts of
-  // arguments.
-  if (argc != 3) {
-    fprintf(stderr, "Syntax: %s <playerbase> <mFlags offset>\r\n", argv[0]);
+int main(const int argc, char* argv[]) {
+    // Print syntax and exit program if we don't have the required amounts of
+    // arguments.
+    if (argc != 4) {
+        fprintf(stderr, "Syntax: %s <playerbase> <mFlags offset> <window name>\r\n", argv[0]);
 
-    return EXIT_FAILURE;
-  }
-
-  // Get the player base offset from the first argument.
-  // If it fails we print an error and exits.
-  DWORD player_base_offset = 0;
-  if (sscanf_s(argv[1], "%lX", &player_base_offset) != 1) {
-    error("You did not specify playerbase correctly!");
-  }
-
-  // Get the mFlags offset from the second argument.
-  // If it fails we print an error and exits.
-  DWORD m_flags_offset = 0;
-  if (sscanf_s(argv[2], "%lX", &m_flags_offset) != 1) {
-    error("You did not specify mFlags offset correctly!");
-  }
-
-  printf("Using:\r\n  PlayerBase offset = %lX\r\n  mFlags offset = %lX\r\n",
-         player_base_offset, m_flags_offset);
-
-  puts("Start the game now.");
-
-  // Keep looping until we find the l4d2 window.
-  HWND l4d2_hwnd = NULL;
-  while ((l4d2_hwnd = FindWindow(NULL, TEXT("Left 4 Dead 2"))) == NULL) {
-    Sleep(1000);
-  }
-
-  puts("Game found!");
-
-  // Get the process id from the window.
-  DWORD l4d2_process_id = 0;
-  GetWindowThreadProcessId(l4d2_hwnd, &l4d2_process_id);
-
-  // Get a handle to the process with virtual memory read permissions.
-  HANDLE l4d2_process = OpenProcess(PROCESS_VM_READ, FALSE, l4d2_process_id);
-  if (l4d2_process == NULL || l4d2_process_id == 0) {
-    error("Could not open process or get process id!");
-  }
-
-  // Get a handle for looping through loaded modules in the process.
-  HANDLE hSnapshot = CreateToolhelp32Snapshot(
-      TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, l4d2_process_id);
-  if (hSnapshot == NULL) {
-    error("Could not create snapshot!");
-  }
-
-  // Get the client.dll base address.
-  const DWORD client_base_address = get_client_dll_base_address(hSnapshot);
-  CloseHandle(hSnapshot);
-  if (client_base_address == 0) {
-    error("Could not get client.dll base address!");
-  }
-
-  puts("Autobhop is now functional!");
-
-  // Bhop loop
-  DWORD player_base_pointer = 0;
-  DWORD m_flags = 0;
-  while (TRUE) {
-    // Check if the space key is held down.
-    if (GetAsyncKeyState(' ') & 0x8000) {
-      // Read player base pointer
-      ReadProcessMemory(
-          l4d2_process, (char *)client_base_address + player_base_offset,
-          &player_base_pointer, sizeof(player_base_pointer), NULL);
-      // Read the mFlags variable from l4d2
-      ReadProcessMemory(l4d2_process,
-                        (char *)player_base_pointer + m_flags_offset, &m_flags,
-                        sizeof(m_flags), NULL);
-
-      // I have no idea what these values mean i just copied them from another
-      // autobhop program.
-      if (m_flags != 0x80 && m_flags != 0x82 && m_flags != 0x280 &&
-          m_flags != 0x282) {
-        SendMessage(l4d2_hwnd, WM_KEYDOWN, ' ', 0x390000);
-      } else if (m_flags == 0x80 || m_flags == 0x82 || m_flags == 0x280 ||
-                 m_flags == 0x282) {
-        SendMessage(l4d2_hwnd, WM_KEYUP, ' ', 0x390000);
-      }
+        return EXIT_FAILURE;
     }
-    // Yield so we don't take up all cpu resources.
-    Sleep(0);
-  }
+
+    // Get the player base offset from the first argument.
+    // If it fails we print an error and exits.
+    DWORD player_base_offset = 0;
+    if (sscanf_s(argv[1], "%lX", &player_base_offset) != 1) {
+        error("You did not specify playerbase correctly!");
+    }
+
+    // Get the mFlags offset from the second argument.
+    // If it fails we print an error and exits.
+    DWORD m_flags_offset = 0;
+    if (sscanf_s(argv[2], "%lX", &m_flags_offset) != 1) {
+        error("You did not specify mFlags offset correctly!");
+    }
+
+    printf("Using:\r\n  PlayerBase offset = %lX\r\n  mFlags offset = %lX\r\n",
+        player_base_offset, m_flags_offset);
+
+    puts("Start the game now.");
+
+    // Keep looping until we find the l4d2 window.
+    HWND l4d2_hwnd = NULL;
+
+    printf("Using:\r\LOOKING FOR:\r\n  %s\r\n", argv[3]);
+    while ((l4d2_hwnd = FindWindow(NULL, TEXT("Left 4 Dead 2 - Direct3D 9"))) == NULL) {
+        Sleep(1000);
+    }
+
+    puts("Game found!");
+
+    // Get the process id from the window.
+    DWORD l4d2_process_id = 0;
+    GetWindowThreadProcessId(l4d2_hwnd, &l4d2_process_id);
+
+    // Get a handle to the process with virtual memory read permissions.
+    HANDLE l4d2_process = OpenProcess(PROCESS_VM_READ, FALSE, l4d2_process_id);
+    if (l4d2_process == NULL || l4d2_process_id == 0) {
+        error("Could not open process or get process id!");
+    }
+
+    // Get a handle for looping through loaded modules in the process.
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(
+        TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, l4d2_process_id);
+    if (hSnapshot == NULL) {
+        error("Could not create snapshot!");
+    }
+
+    // Get the client.dll base address.
+    const DWORD client_base_address = get_client_dll_base_address(hSnapshot);
+    CloseHandle(hSnapshot);
+    if (client_base_address == 0) {
+        error("Could not get client.dll base address!");
+    }
+
+    puts("Autobhop is now functional!");
+
+    DWORD player_base_pointer = 0;
+    DWORD m_flags = 0;
+    while (TRUE) {
+        // Check if the space key is held down.
+        if (GetAsyncKeyState(' ') & 0x8000) {
+            // Bhop loop
+            // loop while the space key is held down.
+            while (GetAsyncKeyState(' ') & 0x8000) {
+                // Read player base pointer
+                ReadProcessMemory(
+                    l4d2_process, (char*)client_base_address + player_base_offset,
+                    &player_base_pointer, sizeof(player_base_pointer), NULL);
+                // Read the mFlags variable from l4d2
+                ReadProcessMemory(l4d2_process,
+                    (char*)player_base_pointer + m_flags_offset, &m_flags,
+                    sizeof(m_flags), NULL);
+
+                // I have no idea what these values mean i just copied them from another
+                // autobhop program.
+                if (m_flags != 0x80 && m_flags != 0x82 && m_flags != 0x280 &&
+                    m_flags != 0x282) {
+                    SendMessage(l4d2_hwnd, WM_KEYDOWN, ' ', 0x390000);
+                }
+                else if (m_flags == 0x80 || m_flags == 0x82 || m_flags == 0x280 ||
+                    m_flags == 0x282) {
+                    SendMessage(l4d2_hwnd, WM_KEYUP, ' ', 0x390000);
+                }
+                // Yield so we don't take up all cpu resources.
+                Sleep(0);
+                //usleep(0);
+            }
+        }
+        else {
+            Sleep(1);
+        }
+    }
 }
